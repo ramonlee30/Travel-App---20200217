bd18188aac2bc17b9c91a304fd71c24eebe9ae8c
Lee Maximchuk <48163295+ramonlee30@users.noreply.github.com>
2020-02-17 13:07:01 -0800

    Add files via upload

---
+++ src/client/index.js
@@ -0,0 +1,14 @@
+import { checkForLocation } from './js/locationChecker'
+import { handleSubmit } from './js/formHandler'
+import { start, stopTimer } from './js/countdown'
+
+import './styles/base.scss'
+import './styles/header.scss'
+import './styles/resets.scss'
+
+export {
+  checkForLocation, 
+  handleSubmit,
+  start,
+  stopTimer,
+}
